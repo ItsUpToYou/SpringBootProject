a3d03551d139bfa26b9448872410c926340a18d4
Velko <v_kapitanov@yahoo.com>
2022-12-08 16:23:26 +0200

    adding excel export options

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserCsvExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserCsvExporter.java
@@ -14,30 +14,23 @@ import org.supercsv.prefs.CsvPreference;
 
 import com.shopme.common.entity.User;
 
-public class UserCsvExporter {
+public class UserCsvExporter extends AbstractExporter {
 
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
-		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-		String timeStamp = dateFormatter.format(new Date());
-		String fileName = "users_" + timeStamp + ".csv";
-		response.setContentType("text/csv");
-
-		String headerKey = "Content-Disposition";
-		String headerValue = "attachment; fileName=" + fileName;
-		response.setHeader(headerKey, headerValue);
-
+		super.setResponseHeader( response,  "text/csv",".csv");
+		
 		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
 
 		String[] csvHeader = { "User ID", "Email", "First Name", "Last Name", "Roles", "Enabled" };
 		String[] fieldMapping = { "id", "email", "firstName", "lastName", "roles", "enabled" };
 		csvWriter.writeHeader(csvHeader);
 
-		for (User user:listUsers){
+		for (User user : listUsers) {
 
-			csvWriter.write(user,fieldMapping);
+			csvWriter.write(user, fieldMapping);
 		}
-		
+
 		csvWriter.close();
-		
+
 	}
 }
